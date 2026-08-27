0b6f8c08dcd44f49cdb87a0d5ccbda7c92bf47fe
hieptt <hieptientran196@gmail.com>
2026-08-27 22:36:32 +0700

    2 pointers

---
+++ leetcode/solutions/0680_is_palindrome.py
@@ -0,0 +1,21 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        def is_paindrom(l, r):
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+
+                l += 1
+                r -= 1
+            return True
+
+        l, r = 0, len(s) -1
+
+        while l < r:
+            if s[l] != s[r]:
+                return is_paindrom(l+1, r) or is_paindrom(l, r-1)
+
+            l += 1
+            r -= 1
+
+        return True
