05d491e1757cf0edf6be83e9c8677858b499ef94
unknown <hieptientran196@gmail.com>
2026-09-19 17:06:06 +0700

    hash map

---
+++ leetcode/solutions/0442_find_duplicat.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findDuplicates(self, nums: list[int]) -> list[int]:
+        result = []
+
+        for num in nums:
+            idx = abs(num) - 1
+
+            if nums[idx] < 0:
+                result.append(abs(num))
+            else:
+                nums[idx] = -nums[idx]
+
+        return result
